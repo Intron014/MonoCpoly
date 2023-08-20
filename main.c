d3e5de62760c62d20d2de022d90d4c5f75a8592e
Jorge Benjumea Burillo <me@intron014.com>
2023-08-20 18:14:01 +0200

    IDE doesn't yell at me anymore

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#DEFINE HAS_OWNER_DEFAULT -1
-#DEFINE IS_MORTGAGED_DEFAULT 0
-#DEFINE HOUSE_DEFAULT 0
+#include <stdbool.h>
+#include <string.h>
+
+#define HAS_OWNER_DEFAULT -1
+#define IS_MORTGAGED_DEFAULT 0
+#define HOUSE_DEFAULT 0
 
 enum CardType {
-    PROPERY,
+    PROPERTY,
     TRAIN_STATION,
     SPECIAL
 };
@@ -30,13 +33,13 @@ union CardProperties {
         int houses; // 0 - No house // 1 to 4 - House // 5 - Hotel // 
         int precioXcasa;
         int rent[6]
-    };
+    } Property;
     struct Train_station{ 
     /* The prices will be determined by the 
     train station flow, looking at the flags
     found on the players */
         int has_owner;
-    };
+    } Train_station;
     struct Special{
     /*
     0 - Salida
@@ -49,12 +52,12 @@ union CardProperties {
     7 - Parking gratuito
     */
         int type;
-    };
+    } Special;
 };
 
 struct Card {
     enum CardType type;
-    enum CardProperties properties;
+    union CardProperties properties;
     char nombre[50];
 };
 
@@ -72,8 +75,8 @@ struct { // Players
     bool has_electric;
 };
 
-void card_file_gen(int why) {
-    // why 0 - file not found // why 1 - edit struct // 
+int card_file_gen(int why) {
+    // why 0 - file not found // why 1 - edit struct //
     struct Card card[40];
     int cnt, ans, aux;
     printf("Master card.bin Editor\n");
@@ -83,53 +86,38 @@ void card_file_gen(int why) {
         }
     } 
     for(cnt = 0; cnt<40 ; cnt++){
-         printf("Editando la casilla %i\n
-         Nombre (if any): %s\n
-         Introduzca el tipo de casilla\n
-         1 - Propiedad\n
-         2 - Estacion de tren\n
-         3 - Especial\n
-         --> ", cnt, card[cnt].nombre);
+         printf("Editando la casilla %i\nNombre (if any): %s\nIntroduzca el tipo de casilla\n1 - Propiedad\n2 - Estacion de tren\n3 - Especial\n--> ", cnt, card[cnt].nombre);
          scanf("%i", &ans);
-         fflish(stdin);
-         if(ans==1){ 
-            card[cnt].type = PROPERTY;
-            card[cnt].properties.Property.has_owner = HAS_OWNER_DEFAULT;
-            card[cnt].properties.Property.is_mortgaged = IS_MORTGAGED_DEFAULT;
-            card[cnt].properties.Property.houses = HOUSE_DEFAULT;
-            printf("Nombre: ");
-            gets(card[cnt].nombre);
-            printf("Precio: ");
-            scanf("%i", &card[cnt].properties.Property.precio);
-            printf("Mortgage: ");
-            scanf("%i", &card[cnt].properties.Property.mortgage);
-            printf("Color: ");
-            scanf("%i", &card[cnt].properties.Property.color);
-            printf("Precio por casa: ");
-            scanf("%i", &card[cnt].properties.Property.precioXcasa);
-            for(aux = 0; aux < 6 ; aux++){
-                printf("Alquiler %i: ", aux);
-                scanf("%i", &card[cnt].properties.Property.rent[aux]);
-        };
+         fflush(stdin);
+         if(ans==1) {
+             card[cnt].type = PROPERTY;
+             card[cnt].properties.Property.has_owner = HAS_OWNER_DEFAULT;
+             card[cnt].properties.Property.is_mortgaged = IS_MORTGAGED_DEFAULT;
+             card[cnt].properties.Property.houses = HOUSE_DEFAULT;
+             printf("Nombre: ");
+             fgets(card[cnt].nombre, sizeof(card[cnt].nombre), stdin);
+             printf("Precio: ");
+             scanf("%i", &card[cnt].properties.Property.precio);
+             printf("Mortgage: ");
+             scanf("%i", &card[cnt].properties.Property.mortgage);
+             printf("Color: ");
+             scanf("%i", &card[cnt].properties.Property.color);
+             printf("Precio por casa: ");
+             scanf("%i", &card[cnt].properties.Property.precioXcasa);
+             for (aux = 0; aux < 6; aux++) {
+                 printf("Alquiler %i: ", aux);
+                 scanf("%i", &card[cnt].properties.Property.rent[aux]);
+             }
+         }
         else if(ans==2){
             card[cnt].type = TRAIN_STATION;
             card[cnt].properties.Train_station.has_owner = HAS_OWNER_DEFAULT;
             printf("Nombre: ");
-            gets(card[cnt].nombre);
-        };
+            fgets(card[cnt].nombre, sizeof(card[cnt].nombre), stdin);
+        }
         else if(ans==3){
             card[cnt].type = SPECIAL;
-            printf("Editando la casilla especial %i\n
-            Introduzca el tipo de casilla\n
-            0 - Salida\n
-            1 - Carcel (Visita)\n
-            2 - Suerte\n
-            3 - Caja de la comunidad\n
-            4 - Impuesto pre-salida\n
-            5 - Impuesto pos-salida\n
-            6 - Go to jail\n
-            7 - Parking gratuito\n
-            --> ", cnt);
+            printf("Editando la casilla especial %i\nIntroduzca el tipo de casilla\n0 - Salida\n1 - Carcel (Visita)\n2 - Suerte\n3 - Caja de la comunidad\n4 - Impuesto pre-salida\n5 - Impuesto pos-salida\n6 - Go to jail\n7 - Parking gratuito\n--> ", cnt);
             scanf("%i", &card[cnt].properties.Special.type);
             fflush(stdin);
             // Switch here para poner nombre (el mismo que el tipo de casilla)
